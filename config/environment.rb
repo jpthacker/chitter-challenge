74c4df8b6c9cbd36d15ec52c3b7b49cf7486fc82
Jack Thacker <jackthacker89@gmail.com>
2023-03-30 18:13:41 +0100

    Set up database environment

---
+++ config/environment.rb
@@ -0,0 +1,22 @@
+# get the path of the root of the app
+# assuming that this file is one folder down in config/
+APP_ROOT = File.expand_path("..", __dir__)
+
+# require the controller(s)
+# says that app.rb is in the root
+Dir.glob(File.join(APP_ROOT, "app", "*.rb")).each { |file| require file }
+
+# require the model(s)
+# models stored in lib/
+Dir.glob(File.join(APP_ROOT, "lib", "*.rb")).each { |file| require file }
+
+# require database configurations
+# the database setup files that were just made in config/
+require File.join(APP_ROOT, "config", "database")
+
+class Chitter < Sinatra::Base
+  set :method_override, true
+  set :root, APP_ROOT
+  set :views, File.join(APP_ROOT, "views")
+  set :public_folder, File.join(APP_ROOT, "public")
+end
